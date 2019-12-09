55f98bfa8975fc0d58df7899e5eb4910d8ca4e85
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-12-09 14:25:45 +0200

    uploading file in ruby

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'resumes/index'
+  get 'resumes/new'
+  get 'resumes/create'
+  get 'resumes/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :resumes
+  root "resumes#index" 
+end
